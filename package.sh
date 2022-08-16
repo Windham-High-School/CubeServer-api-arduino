b7939d99a20fc78d14a3a138f4bcacf7e043d33d
Joseph Freeston <snorklerjoe@gmail.com>
2022-08-16 11:55:01 -0400

    Improved packaging stuff

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Package the library into a .zip file
 
-source library.properties
+source library.properties 2>/dev/null
 
 if [ -f "client_config.h" ]; then
     echo "Building in specified client config..."
@@ -10,4 +10,5 @@ else
     cp server_options_default.h server_options.h
 fi
 
-zip -r ${name}.zip .
+zip -r ${name}.zip . 2>&1 > /dev/null
+echo ${name}.zip
